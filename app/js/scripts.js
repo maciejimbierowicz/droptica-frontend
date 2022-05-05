1dfcbe77396af1f14f455c12d260343fa3e662cf
Imbir <imbierowicz.maciej@gmail.com>
2022-05-06 01:46:32 +0200

    modified complete version

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,18 +1,19 @@
-let searchBar = document.querySelector("#search-bar")
-let searchButton = document.querySelector("#search-button")
-let mobileSearchButton = document.querySelector("#mobile-search-button")
-let mobileSideMenu = document.querySelector('.mobile-dropdown')
-let checkBox = document.querySelector('#ham-button')
+let searchBar = document.querySelector("#search-bar");
+let searchButton = document.querySelector("#search-button");
+let mobileSearchButton = document.querySelector("#mobile-search-button");
+let mobileSideMenu = document.querySelector('.mobile-dropdown');
+let checkBox = document.querySelector('#ham-button');
+let mobileCloseButton = document.querySelector('#close-img');
 
-let aboutDropdown = document.querySelector('.about-section')
-let aboutButton = document.querySelector('#about-button')
+let aboutDropdown = document.querySelector('.about-section');
+let aboutButton = document.querySelector('#about-button');
 
-let nextButton = document.querySelector("#next")
-let previousButton = document.querySelector("#previous")
+let nextButton = document.querySelector("#next");
+let previousButton = document.querySelector("#previous");
 
-let mainBackground = document.querySelector(".main-section")
-let title = document.querySelector(".main-section h2")
-let info = document.querySelector("#information")
+let mainBackground = document.querySelector(".main-section");
+let title = document.querySelector(".main-section h2");
+let info = document.querySelector("#information");
 
 let backgrounds = ['url(../images_content/slider1.png)', 'url(../images_content/slider2.png)', 'url(../images_content/slider3.png)']
 let text = [{title: "DUNLAVIN CHURCH RESTORATION", info: "Sed feugiat imperdiet porttitor.Nulla lectus turpis, placerat ut sapien eu,pellentesque volutpat tellus. Nullam vel justo eu enim feugiat venenatis tristique vitae ligula. Aliquam semper interdum luctus"},
@@ -21,69 +22,69 @@ let text = [{title: "DUNLAVIN CHURCH RESTORATION", info: "Sed feugiat imperdiet 
 
 function showSearchBar() {
     if (searchBar.style.display === 'flex') {
-        searchBar.style.display = 'none'
-        searchButton.src = "../images_content/search_icon.png"
-        mobileSearchButton.src = "../images_content/search_icon.png"
+        searchBar.style.display = 'none';
+        searchButton.src = "../images_content/search_icon.png";
+        mobileSearchButton.src = "../images_content/search_icon.png";
     } else {
         searchBar.style.display = 'flex'
-        searchButton.src = "../images_content/mobile_menu_close.png"
-        mobileSearchButton.src = "../images_content/mobile_menu_close.png"
+        searchButton.src = "../images_content/mobile_menu_close.png";
+        mobileSearchButton.src = "../images_content/mobile_menu_close.png";
     }
-    
-}
+};
 
 function showSideMenu() {
     if (mobileSideMenu.style.width == '80%') {
-        mobileSideMenu.style.width = '0'
+        mobileSideMenu.style.width = '0';
     } else {
-        mobileSideMenu.style.width = '80%'
-        
+        mobileSideMenu.style.width = '80%';
+        mobileCloseButton.style.display = 'block';
     }
-}
+};
 
 function showMobileDropdown() {
     if (aboutDropdown.style.height == '175px') {
-        aboutDropdown.style.height = '0'
-        aboutButton.innerHTML = '+'
+        aboutDropdown.style.height = '0';
+        aboutButton.innerHTML = '+';
     } else {
-        aboutDropdown.style.height = '175px'
-        aboutButton.innerHTML = '-'
+        aboutDropdown.style.height = '175px';
+        aboutButton.innerHTML = '-';
     }
-}
+};
 
-let x = 0
+let x = 0;
 function nextSlide() {
-    x += 1
+    x += 1;
     if (x > 2) {
-        x=0
-    }
-    mainBackground.style.backgroundImage = backgrounds[x]
-    title.innerHTML = text[x].title
-    info.innerHTML = text[x].info
+        x=0;
+    };
+    mainBackground.style.backgroundImage = backgrounds[x];
+    title.innerHTML = text[x].title;
+    info.innerHTML = text[x].info;
 }
 
 function previousSlide() {
-    x -= 1
+    x -= 1;
     if (x < 0) {
-        x=2
+        x=2;
     }
-    mainBackground.style.backgroundImage = backgrounds[x]   
-    title.innerHTML = text[x].title  
-    info.innerHTML = text[x].info
+    mainBackground.style.backgroundImage = backgrounds[x];   
+    title.innerHTML = text[x].title;
+    info.innerHTML = text[x].info;
 }
 
 document.addEventListener('mouseup', function(e) {
     if (!mobileSideMenu.contains(e.target)) {
         mobileSideMenu.style.width = '0';
+        mobileCloseButton.style.display = 'none';
     }
 });
 
 
 
-searchButton.addEventListener('click', showSearchBar)
-mobileSearchButton.addEventListener('click', showSearchBar)
-nextButton.addEventListener('click', nextSlide)
-previousButton.addEventListener('click', previousSlide)
-checkBox.addEventListener('click', showSideMenu)
-aboutButton.addEventListener('click', showMobileDropdown)
+searchButton.addEventListener('click', showSearchBar);
+mobileSearchButton.addEventListener('click', showSearchBar);
+nextButton.addEventListener('click', nextSlide);
+previousButton.addEventListener('click', previousSlide);
+checkBox.addEventListener('click', showSideMenu);
+aboutButton.addEventListener('click', showMobileDropdown);
 
